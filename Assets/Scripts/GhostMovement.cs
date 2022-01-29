a3b13b190d9c4649ef8c0578e37d32e9697e857a
gugation <gugump10@gmail.com>
2022-01-29 16:56:48 -0300

    Player and ghost movement offline

---
+++ Assets/Scripts/GhostMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        float y = Input.GetAxis("YVertical");
+
+        CharacterMovement(x, z, y);
+    }
+
+    void CharacterMovement(float x, float z, float y)
+    {
+        Vector3 move = transform.right * x + transform.forward * z + transform.up * y;
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
