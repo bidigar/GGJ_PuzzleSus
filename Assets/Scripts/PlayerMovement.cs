dcc624ede8ec46dd3617efb278caeed619e38207
bidigar <digomp10@gmail.com>
2022-01-29 00:00:32 -0300

    InitialCommit
    - Bora baea

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    [SerializeField] float speed;
+    float gravity = -9.81f;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = 0.4f;
+    [SerializeField] LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        GroundCheck();
+        CharacterMovement(x, z);
+        CharacterGravity();
+    }
+
+    void GroundCheck()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = gravity;
+        }
+    }
+
+    void CharacterMovement(float x, float z)
+    {
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+    void CharacterGravity()
+    {
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
