0033fd45a2b7bc17cf4ceb94efa3c092728e4c8a
bidigar <digomp10@gmail.com>
2022-01-30 17:16:39 -0300

    Color puzzle + Fade

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -9,6 +9,7 @@ public class InteractableObject : MonoBehaviour
     [SerializeField] UnityEvent objectUsed = new UnityEvent();
     public bool wasUsed = false;
     public bool isDestroyable = false;
+    public bool isUsed = false;
     UnityEvent<GameObject> wasClicked = new UnityEvent<GameObject>();
     public bool needKeyItem = true;
 
@@ -21,7 +22,7 @@ public class InteractableObject : MonoBehaviour
     public void ObjectInteraction(ItemManager manager) {
         if (!wasUsed && ((needKeyItem && manager.UseHoldingItem(this)) || !needKeyItem)) {
             objectUsed.Invoke();
-            wasUsed = true;
+            if (isUsed) wasUsed = true;
             if (isDestroyable) Destroy(this.gameObject);
         }
     }
---
+++ Assets/Scripts/Puzzles/ColorBox/ColorBoxManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorBoxManager : MonoBehaviour
+{
+    [SerializeField] GameObject holeOne;
+    [SerializeField] GameObject holeTwo;
+    [SerializeField] GameObject holeThree;
+    MeshRenderer holeOne_MeshRenderer;
+    MeshRenderer holeTwo_MeshRenderer;
+    MeshRenderer holeThree_MeshRenderer;
+
+    private void Start()
+    {
+        holeOne_MeshRenderer = holeOne.GetComponent<MeshRenderer>();
+        holeTwo_MeshRenderer = holeTwo.GetComponent<MeshRenderer>();
+        holeThree_MeshRenderer = holeThree.GetComponent<MeshRenderer>();
+    }
+
+    public void GhostTouch()
+    {
+        holeOne.SetActive(true);
+        holeTwo.SetActive(true);
+        holeThree.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (holeOne_MeshRenderer.material.color == Color.Lerp(Color.red, Color.yellow, 0.5f) && holeTwo_MeshRenderer.material.color == Color.green && holeThree_MeshRenderer.material.color == Color.blue)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
